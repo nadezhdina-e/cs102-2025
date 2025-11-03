06909ddbc71bfd7d2af890354fe5d2ee5becd246
nadezhdina-e <elnadejdina@gmail.com>
2025-11-03 22:03:13 +0800

    Реализованы функции encrypt_vigenere() и decrypt.vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,6 +1,7 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
+
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,13 +10,29 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    abc_length = 26
+    for i in range(len(plaintext)):
+        j = i % len(keyword)
+        if keyword[j].isalpha():
+            if keyword[j].isupper():
+                shift = ord(keyword[j]) - ord('A')
+            else:
+                shift = ord(keyword[j]) - ord('a')
+        if plaintext[i].isalpha():
+            if plaintext[i].isupper():
+                start = ord('A')
+            else:
+                start = ord('a')
+            ciphertext += chr((ord(plaintext[i]) - start + shift) % abc_length + start)
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -24,5 +41,21 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    abc_length = 26
+    for i in range(len(ciphertext)):
+        j = i % len(keyword)
+        if keyword[j].isalpha():
+            if keyword[j].isupper():
+                shift = ord(keyword[j]) - ord('A')
+            else:
+                shift = ord(keyword[j]) - ord('a')
+        if ciphertext[i].isalpha():
+            if ciphertext[i].isupper():
+                start = ord('A')
+            else:
+                start = ord('a')
+            plaintext += chr((ord(ciphertext[i]) - start - shift) % abc_length + start)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
+
