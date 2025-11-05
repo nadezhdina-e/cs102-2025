8b3be4b36cb031df2acb3b9f0ccd0224939710ce
nadezhdina-e <elnadejdina@gmail.com>
2025-11-05 13:47:23 +0800

    edited

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -51,6 +51,3 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
         else:
             plaintext += char
     return plaintext
-
-if __name__ == "__main__":
-    print(decrypt_caesar("sbwkrq"))
