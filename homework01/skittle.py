8bd4649d270c2e2612ce2bd2d9c6e41353729e69
nadezhdina-e <elnadejdina@gmail.com>
2025-11-05 13:06:38 +0800

    file reformatted

--- homework01/skittle.py
+++ homework01/skittle.py
@@ -1,29 +1,40 @@
+"""
+Модуль реализует кодировку и декодировку строки по методу Скитла
+"""
+
+
 def encrypt_scytale(plaintext, n):
+    """
+    Функция записывает строку в виде матрицы и переписывает ее в порядке,
+    соответствующем методу шифрования Скитла
+    """
     matrix = []
     j = 0
     while j < len(plaintext):
         temporary_row = []
         for i in range(n):
-            if (i+j) < len(plaintext):
-                if plaintext[i + j] == ' ':
-                    temporary_row.append('_')
+            if (i + j) < len(plaintext):
+                if plaintext[i + j] == " ":
+                    temporary_row.append("_")
                 else:
-                    temporary_row.append(plaintext[i+j])
+                    temporary_row.append(plaintext[i + j])
             else:
-                temporary_row.append('*')
+                temporary_row.append("*")
         matrix.append(temporary_row)
         j += n
     ciphertext = []
     for k in range(n):
         for m in range(len(matrix)):
             ciphertext.append(matrix[m][k])
-    ciphertext = ''.join(ciphertext)
+    ciphertext = "".join(ciphertext)
     return ciphertext
 
-line = 'НАС АТАКУЮТ'
-print(encrypt_scytale(line,4))
 
 def decrypt_scytale(ciphertext, n):
+    """
+    Функция записывает зашифрованную строку в виде матрицы так,
+    чтобы при прямом чтении матрицы получалось декодированное сообщение
+    """
     matrix = []
     j = 0
     if len(ciphertext) % n == 0:
@@ -33,26 +44,23 @@ def decrypt_scytale(ciphertext, n):
     while j < m:
         temporary_row = []
         for i in range(n):
-            if (i*(n-1) + j) < len(ciphertext):
-                if ciphertext[i*(n-1) + j] == ' ':
-                    temporary_row.append('_')
+            if (i * (n - 1) + j) < len(ciphertext):
+                if ciphertext[i * (n - 1) + j] == " ":
+                    temporary_row.append("_")
                 else:
-                    temporary_row.append(ciphertext[i*(n-1) + j])
+                    temporary_row.append(ciphertext[i * (n - 1) + j])
             else:
-                temporary_row.append('*')
+                temporary_row.append("*")
         matrix.append(temporary_row)
         j += 1
     plaintext = []
     for k in range(len(matrix)):
         for m in range(n):
-            if (matrix[k][m]) == '_':
-                plaintext.append(' ')
-            elif (matrix[k][m]) == '*':
+            if (matrix[k][m]) == "_":
+                plaintext.append(" ")
+            elif (matrix[k][m]) == "*":
                 continue
             else:
                 plaintext.append(matrix[k][m])
-    plaintext = ''.join(plaintext)
+    plaintext = "".join(plaintext)
     return plaintext
-
-to_decrypt_line = 'НАУАТЮСАТ_К*'
-print(decrypt_scytale(to_decrypt_line,4))
